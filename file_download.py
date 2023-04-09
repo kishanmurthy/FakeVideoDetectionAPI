4ec71627916ecdf5ef333ff572dd1a5d774c2e24
ShreyaPrabhu <shreyaprabhu96@gmail.com>
2023-04-09 11:46:02 -0700

    Change status values

--- file_download.py
+++ file_download.py
@@ -6,25 +6,25 @@ import asyncio
 from database_access import update_request_status
 
 async def downloadYoutubeVideos(request_id, video_id):
-    update_request_status(request_id, "DOWNLOADING VIDEO")
+    update_request_status(request_id, "DOWNLOADING")
     base_url = "https://www.youtube.com/watch?"
     yt = YouTube(base_url + "v=" + video_id)
     yt = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').asc().first()
     if not os.path.exists("videos/"):
         os.makedirs("videos/")
     yt.download("videos/")
-    update_request_status(request_id, "DOWNLOADED VIDEO")
+    update_request_status(request_id, "DOWNLOADED")
 
 
 async def downloadYoutubeShorts(request_id, video_id):
-    update_request_status(request_id, "DOWNLOADING SHORTS")
+    update_request_status(request_id, "DOWNLOADING")
     base_url = "https://www.youtube.com/shorts/"
     yt = YouTube(base_url + video_id)
     yt = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').asc().first()
     if not os.path.exists("shorts/"):
         os.makedirs("shorts/")
     yt.download("shorts/")
-    update_request_status(request_id, "DOWNLOADED SHORTS")
+    update_request_status(request_id, "DOWNLOADED")
 
 # downloadYoutubeVideos("_fbq8RaKlxI")
 # downloadYoutubeShorts("Jr5-KtUAFzE")
